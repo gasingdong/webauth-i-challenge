ae38f644a1a9b17167d970a9079b7b1752f85a60
Brandon Dong <gasingdong@gmail.com>
2019-09-16 12:35:36 -0700

    create database

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,20 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3',
+      filename: './database/auth.db3',
+    },
+    pool: {
+      afterCreate: (conn: { run: Function }, done: Function): void => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
     },
   },
 };
